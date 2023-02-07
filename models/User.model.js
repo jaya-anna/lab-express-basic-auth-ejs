b8a705b06d55ea2e90a084658fda8728db84e943
Anna Hartmann <anna.ig.hartmann@gmail.com>
2023-02-07 22:26:40 +0100

    iteration1 and 2 done

--- models/User.model.js
+++ models/User.model.js
@@ -4,9 +4,15 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    trim: true,
+    required: true,
+    unique: true,
   },
-  password: String
+
+  passwordHash: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = model("User", userSchema);
